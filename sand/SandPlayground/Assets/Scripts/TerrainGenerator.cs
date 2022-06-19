b1cc5a071c98ffdfd96bd52135a61b9d517145eb
Pascal Schiessle <pascal.schiessle@web.de>
2022-06-19 17:14:18 +0200

    Wild terrain painting (some math doesn´t fit yet)

--- sand/SandPlayground/Assets/Scripts/TerrainGenerator.cs
+++ sand/SandPlayground/Assets/Scripts/TerrainGenerator.cs
@@ -1,10 +1,12 @@
+using System;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class TerrainGenerator : MonoBehaviour
 {
     [SerializeField] private Terrain _terrain;
-    [SerializeField] private int width = 256; //x-axis of the terrain
-    [SerializeField] private int height = 256; //z-axis
+    //[SerializeField] private int width = 256; //x-axis of the terrain
+    //[SerializeField] private int height = 256; //z-axis
 
     [SerializeField] private float sinWaveLength = 1;
     [SerializeField] private float sinWaveAmplitude = 1;
@@ -13,13 +15,18 @@ public class TerrainGenerator : MonoBehaviour
 
     [SerializeField] private float scale = 20f;
 
+    [SerializeField] private int cellSize = 20;
+    [SerializeField] private int cellCountWidth = 10;
+    [SerializeField] private int cellCountHeight = 10;
+
+    [SerializeField] private float tilingCutoff = 1f;
     [SerializeField] private float offsetX = 0f;
     [SerializeField] private float offsetY = 0f;
 
     private void Start()
     {
-        offsetX = Random.Range(0f, 9999f);
-        offsetY = Random.Range(0f, 9999f);
+        //offsetX = Random.Range(0f, 9999f);
+        //offsetY = Random.Range(0f, 9999f);
     }
 
     private void Update()
@@ -29,33 +36,75 @@ public class TerrainGenerator : MonoBehaviour
 
     TerrainData GenerateTerrain (TerrainData terrainData)
     {
-        terrainData.heightmapResolution = width + 1;
+        var width = cellSize * cellCountWidth;
+        var height = cellSize * cellCountHeight;
+        terrainData.heightmapResolution = 65;
+        terrainData.alphamapResolution = 65;
         terrainData.size = new Vector3(width, depth, height);
 
-        terrainData.SetHeights(0, 0, GenerateHeights());
+        terrainData.SetHeights(0, 0, GenerateHeights(terrainData, 65,65));
+        terrainData.SetAlphamaps(0,0, GenerateSplatmapData(terrainData, cellCountWidth, cellCountHeight));
         return terrainData;
     }
 
-    float[,] GenerateHeights()
+    /**
+     * Based on https://docs.unity3d.com/2021.3/Documentation/ScriptReference/TerrainData.SetAlphamaps.html
+     */
+    float[,,] GenerateSplatmapData(TerrainData terrainData, float cellCountWidth, float cellCountHeight)
     {
-        float[,] heights = new float[width, height];
-        for(int x = 0; x < width; x++)
+        
+        var splatmapData = new float[terrainData.alphamapWidth, terrainData.alphamapHeight, terrainData.alphamapLayers];
+
+        var cellExtendWidth = terrainData.alphamapWidth / cellCountWidth;
+        var cellExtendHeight = terrainData.alphamapHeight / cellCountHeight;
+        
+        for (var y = 0; y < terrainData.alphamapHeight; y++)
         {
-            for (int y = 0; y < height; y++)
+            var texPosY = ((float)(y) / terrainData.alphamapHeight);
+            var iY = (int)((texPosY / cellExtendWidth) * cellCountHeight);
+            
+            for (var x = 0; x < terrainData.alphamapWidth; x++)
             {
-                heights[x, y] = CalculateHeight(x, y);
+                splatmapData[x, y, 0] = 1;
+                var texPosX = ((float)(x) / terrainData.alphamapWidth);
+                var iX = (int)((texPosX / cellExtendHeight) * cellCountWidth);
+
+                splatmapData[x, y, 1] = GetTileColor(iX, iY);
+            }    
+        }
+
+        return splatmapData;
+    }
+
+    int GetTileColor(int iX, int iY)
+    {
+        if (iX % 2 == 0)
+        {
+            return iY % 2 == 0 ? 1 : 0;
+        }
+        return iY % 2 == 0 ? 0 : 1;
+    }
+
+    float[,] GenerateHeights(TerrainData terrainData, int width, int height)
+    {
+        var depths = new float[width, height];
+        for(var x = 0; x < width; x++)
+        {
+            for (var y = 0; y < height; y++)
+            {
+                depths[x, y] = CalculateHeight(width, height, x,y);
             }
         }
 
-        return overlayWidthSin(heights);
+        return OverlayWidthSin(depths);
     }
 
-    float[,] overlayWidthSin(float[,] noise)
+    float[,] OverlayWidthSin(float[,] noise)
     {
         float sinState = 0;
-        for(int x = 0; x < width; x++)
+        for(var x = 0; x < noise.GetLength(0); x++)
         {
-            for (int y = 0; y < height; y++)
+            for (var y = 0; y < noise.GetLength(1); y++)
             {
                 noise[x, y] += ( ((Mathf.Sin(sinState) / 2) + 1 ) * sinWaveAmplitude) ;
             }
@@ -66,11 +115,11 @@ public class TerrainGenerator : MonoBehaviour
         return noise;
     }
 
-    float CalculateHeight (int x, int y)
+    float CalculateHeight (int width, int height, float x, float y)
     {
-        float xCoord = (float)x / width * scale + offsetX;
-        float yCoord = (float)y / height * scale + offsetY;
-
+        var xCoord = x / width * scale + offsetX;
+        var yCoord = y / height * scale + offsetY;
+        
         return Mathf.PerlinNoise(xCoord, yCoord);
     }
 }
